c64e41d170183bad253a250b6ebde7e2493b647e
Eoczi <123911747+KocziEduard@users.noreply.github.com>
2023-11-18 15:23:14 +0200

    marker set

--- frontend/src/app/components/google-maps-demo/google-maps-demo.component.ts
+++ frontend/src/app/components/google-maps-demo/google-maps-demo.component.ts
@@ -11,39 +11,25 @@ import * as L from 'leaflet';
   templateUrl: './google-maps-demo.component.html',
   styleUrl: './google-maps-demo.component.css'
 })
+
 export class GoogleMapsDemoComponent implements OnInit{
   //@ts-ignore
-  private map: L.Map;
-  private centroid: L.LatLngExpression = [42.3601, -71.0589]; //
-
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: this.centroid,
-      zoom: 12
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 10,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    // create 5 random jitteries and add them to map
-    const jittery = Array(5).fill(this.centroid).map( 
-        x => [x[0] + (Math.random() - .5)/10, x[1] + (Math.random() - .5)/10 ]
-      ).map(
-        x => L.marker(x as L.LatLngExpression)
-      ).forEach(
-        x => x.addTo(this.map)
-      );
-
-    tiles.addTo(this.map);
-  
-  }
-
-  constructor() { }
-
   ngOnInit(): void {
-    this.initMap();
+    const map = L.map('map').setView([45.760696, 21.226788], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    const marker = L.marker([51.505, -0.09]).addTo(map);
+
+    map.on('click', (e) => {
+      const lat = e.latlng.lat;
+      const lng = e.latlng.lng;
+
+      marker.setLatLng([lat, lng]);
+
+      console.log('Latitude:', lat, 'Longitude:', lng);
+    });
   }
 }
