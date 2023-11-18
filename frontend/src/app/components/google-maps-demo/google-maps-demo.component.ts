fa724ae21bc7f5defcf539b164322658cb179ee9
Eoczi <123911747+KocziEduard@users.noreply.github.com>
2023-11-18 12:42:09 +0200

    FIRST REAL COMMIT

---
+++ frontend/src/app/components/google-maps-demo/google-maps-demo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-google-maps-demo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './google-maps-demo.component.html',
+  styleUrl: './google-maps-demo.component.css'
+})
+export class GoogleMapsDemoComponent {
+
+}
